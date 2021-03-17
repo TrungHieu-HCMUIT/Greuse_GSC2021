6038a7c37863083471f72211b078894e5f51bc0d
Khiem Le <theinnocentman1@gmail.com>
2021-03-17 20:21:11 +0700

    UI(Comp): MyPostsScreen card done

---
+++ lib/components/my_posts_card.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyPostsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 3.0,
+      shadowColor: Colors.black38,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15.0),
+              topRight: Radius.circular(15.0),
+            ),
+            child: Container(
+              height: 200.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/product1.png'),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                          top: 26.0,
+                          left: 10.0,
+                          right: 10.0,
+                          bottom: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black38,
+                            ],
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              backgroundImage:
+                                  AssetImage('assets/images/avatar.png'),
+                              radius: 24.0,
+                            ),
+                            SizedBox(width: 8.0),
+                            Expanded(
+                              child: Text(
+                                'trunghieu',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(35.0)),
+                              padding: EdgeInsets.symmetric(
+                                vertical: 6.0,
+                                horizontal: 15.0,
+                              ),
+                              child: Text(
+                                'Plastic',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    right: 4.0,
+                    top: 4.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(1000.0),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          splashColor: Colors.black26,
+                          highlightColor: Colors.black12,
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            Icons.cancel,
+                            color: Colors.black.withAlpha(150),
+                            size: 34.0,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 12.0,
+              vertical: 5.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Shapoo bottle',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.location,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Text(
+                      'TP HCM',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  'Can be reused',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
