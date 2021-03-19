46824f20c87f19e4cd936f733cbc67a8ce573fb1
Khiem Le <theinnocentman1@gmail.com>
2021-03-19 08:42:38 +0700

    Modify post model

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 class Post {
   final String id;
   final String image;
+  final String material;
   final String name;
   final String location;
   final String description;
@@ -10,6 +11,7 @@ class Post {
   Post({
     this.id,
     @required this.image,
+    @required this.material,
     @required this.name,
     @required this.location,
     @required this.description,
@@ -21,6 +23,7 @@ class Post {
       id: id,
       image: image,
       name: name,
+      material: material,
       location: location,
       description: description,
       'isSaved': isSaved,
@@ -31,6 +34,7 @@ class Post {
     return Post(
       id: json['id'],
       image: json['image'],
+      material: json['material'],
       name: json['name'],
       location: json['location'],
       description: json['description'],
