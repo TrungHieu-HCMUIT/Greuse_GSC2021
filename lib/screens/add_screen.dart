14d609716ec11ae31e7eb1d3f0617041e908dfd5
fantashi099 <52989569+fantashi099@users.noreply.github.com>
2021-03-25 22:39:03 +0700

    update change

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -19,10 +19,7 @@ class AddScreen extends StatefulWidget {
 
 class _AddScreenState extends State<AddScreen> {
   final _firestore = FirebaseFirestore.instance;
-<<<<<<< HEAD
-=======
   final _auth = FirebaseAuth.instance;
->>>>>>> ad3fecbd6fdaec93c57e618bd980229397ebacd1
   final _weightController = TextEditingController();
   final _productNameController = TextEditingController();
   final _descriptionController = TextEditingController();
@@ -30,7 +27,6 @@ class _AddScreenState extends State<AddScreen> {
   final _productNameFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
   final _storage = FirebaseStorage.instance;
-  final _auth = FirebaseAuth.instance;
   var _materials = [
     'Material 1',
     'Material 2',
@@ -160,84 +156,6 @@ class _AddScreenState extends State<AddScreen> {
     }
   }
 
-<<<<<<< HEAD
-  Future<void> uploadFile(String filePath) async {
-    File file = File(filePath);
-
-    try {
-      await _storage.ref('uploads/file-to-upload.png').putFile(file);
-    } on FirebaseException catch (e) {
-      // e.g, e.code == 'canceled'
-    }
-  }
-
-  bool _isFormValid() {
-    bool isValid = true;
-    final productName = _productNameController.text.trim();
-    final description = _descriptionController.text.trim();
-    final weight = _weightController.text.trim();
-    setState(() {
-      _productNameErrorText = null;
-      _descriptionErrorText = null;
-      _weightErrorText = null;
-      _materialErrorText = null;
-
-      if (productName.isEmpty) {
-        isValid = false;
-        _productNameErrorText = "Please enter product's name";
-      }
-      if (description.isEmpty) {
-        isValid = false;
-        _descriptionErrorText = "Please enter description";
-      }
-      if (weight.isEmpty) {
-        isValid = false;
-        _weightErrorText = "Please enter weight";
-      }
-      if (_material == null) {
-        isValid = false;
-        _materialErrorText = "Please choose material";
-      }
-    });
-    return isValid;
-  }
-
-  Future<void> _post() async {
-    if (_isFormValid()) {
-      // await uploadFile(_pickedAssetImages[0].identifier);
-      // await _storage
-      //     .ref('uploads/file-to-upload-1.png')
-      //     .putData(_pickedAssetImages[0].);
-
-      final currentUser = _auth.currentUser;
-      if (currentUser == null) return;
-
-      final user = MUser.User(
-        displayname: currentUser.displayName,
-        email: currentUser.email,
-        avatarURL: 'https://wallpapercave.com/wp/wp7999906.jpg',
-      );
-
-      final post = Post(
-        image:
-            'https://thunggiay.com/wp-content/uploads/2018/10/Mua-thung-giay-o-dau-uy-tin-va-chat-luong1.jpg',
-        material: _material,
-        name: _productNameController.text.trim(),
-        location: "TP HCM",
-        description: _descriptionController.text,
-        weight: double.parse(_weightController.text.trim()),
-      );
-
-      final viewModel = NewsFeedCardVM(user: user, post: post);
-
-      final res = await _firestore.collection("posts").add(
-            viewModel.toJson(),
-          );
-      if (res != null) {
-        await res.update({'id': res.id});
-        _showSucceedDialog(context);
-      }
-=======
   Future<void> _post() async {
     final prodName = _productNameController.text.trim();
     final description = _descriptionController.text.trim();
@@ -259,7 +177,6 @@ class _AddScreenState extends State<AddScreen> {
     if (res != null) {
       await res.update({'id': res.id});
       _showSucceedDialog(context);
->>>>>>> ad3fecbd6fdaec93c57e618bd980229397ebacd1
     }
   }
 
@@ -269,13 +186,7 @@ class _AddScreenState extends State<AddScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingBottomButton(
         label: 'Post',
-<<<<<<< HEAD
-        onPressed: () {
-          _post();
-        },
-=======
         onPressed: _post,
->>>>>>> ad3fecbd6fdaec93c57e618bd980229397ebacd1
       ),
       body: SafeArea(
         child: Padding(
